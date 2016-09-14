1ad73af7c749885cb1a12b537e6784efcb3020ed
JudeNiroshan <judeniroshan123>
2016-09-14 09:42:55 +0530

    Added method comments Set the help URL to github project Wiki page Set variable container restriction to "variable" [setCheck("method")] Added tooltip comments

--- blockly/blocks/containers.js
+++ blockly/blocks/containers.js
@@ -1,26 +1,22 @@
 /**
  * Created by Dinesh Liyanage on 9/10/2016.
+ * @Description : This file contains the block definitions for the blocks used in Lisa.
+ *                If you need to define a new block, create it with Block Factory.
+ * @modified : 09/14/2016
  */
+
 'use strict';
 
 goog.provide('Blockly.Blocks.test');
-
 goog.require('Blockly.Blocks');
 
-// Blockly.Blocks['class-container'] = {
-//     init: function() {
-//         this.appendDummyInput()
-//             .appendField('class')
-//             .appendField(new Blockly.FieldTextInput('Parent'), 'className');
-//         this.appendStatementInput('contents')
-//             .setCheck(['variable-container', 'method-container', 'class-container'])
-//             .setAlign(Blockly.ALIGN_CENTRE);
-//         this.setColour(230);
-//         this.setTooltip('This is a class container');
-//         this.setHelpUrl('http://www.example.com/');
-//     }
-// };
+var tooltip_class_container = 'Container which can hold variable-container, method-container, constructor-container';
+var tooltip_variable_container = 'Container which can hold only variable blocks';
+var tooltip_method_container = 'Container which can hold only method blocks';
 
+/**
+ * This is the definition for the class container block
+ */
 Blockly.Blocks['class-container'] = {
     init: function() {
         this.appendDummyInput()
@@ -34,25 +30,31 @@ Blockly.Blocks['class-container'] = {
             .setAlign(Blockly.ALIGN_CENTRE);
         this.setInputsInline(true);
         this.setColour(240);
-        this.setTooltip('');
-        this.setHelpUrl('http://www.example.com/');
+        this.setTooltip(tooltip_class_container);
+        this.setHelpUrl('https://github.com/dineshLL/lisa/wiki');
     }
 };
 
+/**
+ * This is the definition for the variable container block
+ */
 Blockly.Blocks['variable-container'] = {
     init: function() {
         this.appendStatementInput('variables')
-            .setCheck(null)
+            .setCheck("variable")
             .appendField(new Blockly.FieldDropdown([['private', 'private'], ['public', 'public'], ['protected', 'protected'], ['default', 'default']]), 'access-modifier');
         this.setInputsInline(false);
         this.setPreviousStatement(true, ['class-container', 'variable-container', 'method-container']);
         this.setNextStatement(true, ['class-container', 'variable-container', 'method-container']);
         this.setColour(20);
-        this.setTooltip('');
-        this.setHelpUrl('http://www.example.com/');
+        this.setTooltip(tooltip_variable_container);
+        this.setHelpUrl('https://github.com/dineshLL/lisa/wiki');
     }
 };
 
+/**
+ * This is the definition for the method container block
+ */
 Blockly.Blocks['method-container'] = {
   init: function() {
     this.appendDummyInput()
@@ -62,8 +64,8 @@ Blockly.Blocks['method-container'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, ["method-container", "constructor-container", "variable-container"]);
     this.setColour(65);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+    this.setTooltip(tooltip_method_container);
+    this.setHelpUrl('https://github.com/dineshLL/lisa/wiki');
   }
 };
 
